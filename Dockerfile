b09a0d2c45038bf15734dd720d418bc8a29d5777
Pedro Januário <prnjanuario@gmail.com>
2019-03-19 14:42:17 +0100

    update example

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,11 @@ RUN chown -R node:node $APP_DIR
 
 WORKDIR $APP_DIR
 
-RUN apk add --no-cache make gcc g++ python \
+RUN apk add --no-cache make gcc g++ python zeromq zeromq-dev \
+  && npm install zeromq --zmq-external \
   && npm install \
   && npm cache clean --force\
-  && apk del make gcc g++ python
+  && apk del make gcc g++ python zeromq-dev
 
 ADD . $APP_DIR
 
--- srv.js
+++ srv.js
@@ -17,10 +17,34 @@ sock.on('error', function (err) {
   console.log('srv.error: ', err);
 })
 
+var IDENTITY_FRAME = 0,
+    PROTOCOL_FRAME = 1,
+    TYPE_FRAME     = 2,
+    RID_FRAME      = 3,
+    ADDRESS_FRAME  = 4,
+    HEADERS_FRAME  = 5,
+    STATUS_FRAME   = 6,
+    PAYLOAD_FRAME  = 7;
+
+
 var pingFrames = [
-  "ping"
+  "ZSS:0.0",
+  "REQ",
+  "c1e9a160-4a4a-11e9-a123-afbb82ae3d12",
+  JSON.stringify({
+    "sid": "COMPUTEINSTANCE_PROFILE_CONFIGURATION",
+    "sversion": "*",
+    "verb": "BATCH"
+  }),
+  JSON.stringify({
+    "userId": "5ae857a3f3488d5386684b1f",
+    "tenantId": "58ac2c19f2c169639e74dc19",
+    "X-REQUEST-ID": "c763b7ba28826842b1b72f5e64f55556"
+  }),
+  "",
+  JSON.stringify({sid: 'TEST'})
 ];
 
 setInterval(function() {
   sock.send(pingFrames);
-}, 500);
+}, parseInt(process.env["HEARTBEAT"], 10) || 500);
